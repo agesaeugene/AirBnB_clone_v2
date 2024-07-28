ab6b0a595b3a699dbd9ed2c765fff1213181a1ce
Eugene Agesa <eugenethebandit@gmail.com>
2024-07-28 10:03:01 +0300

    C is fun task

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Staring a web app aplication"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def home():
+    """
+    function to display Hello HBNB
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    printing HBNB to the browser
+    """
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c_with_params(text):
+    """
+    is_cool
+    """
+    text_no_undersore = text.replace('_', ' ')
+    return "c {}".format(text_no_undersore)
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
